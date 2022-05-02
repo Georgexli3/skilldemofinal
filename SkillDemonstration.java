f929253c8be427e22de91a0201ac585bcbc85195
Georgexli3 <103277409+Georgexli3@users.noreply.github.com>
2022-05-01 18:14:53 -0700

    Failing test

--- SkillDemonstration.java
+++ SkillDemonstration.java
@@ -1,3 +1,6 @@
 public class SkillDemonstration{
 
+    public static int subtraction(int x, int y){
+        return(x-y);
+    }
 }
---
+++ SkillDemonstrationTester.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemonstrationTester {
+    @Test
+    public void subtraction() {
+        assertEquals(2, SkillDemonstration.subtraction(1, 1));
+    }
+}
